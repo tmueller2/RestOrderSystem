f9a18387b94c67a729d0dab28cb6d89aac8b6ecf
Spark <Spark@192.168.1.107>
2012-09-18 14:57:43 -0500

    Having null pointer error

--- src/java/edu/wctc/distjava/ch3/controller/RestOrderController2.java
+++ src/java/edu/wctc/distjava/ch3/controller/RestOrderController2.java
@@ -5,6 +5,7 @@ import edu.wctc.distjava.ch3.model.MenuItem;
 import edu.wctc.distjava.ch3.model.OrderService;
 import java.io.IOException;
 import java.util.List;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -55,6 +56,15 @@ public class RestOrderController2 extends HttpServlet {
             }
         }
         //responseCommitted = outputConfirmation(request, response, menuList, orderList, responseCommitted);
-        
+        //if(!responseCommitted) {
+        request.setAttribute("menuList", menuList);
+        request.setAttribute("orderList", orderList);
+
+        // Redirect to destination page
+        RequestDispatcher dispatcher = 
+                getServletContext().getRequestDispatcher("/orderResult.jsp");
+        dispatcher.forward(request, response);
+        //}
     }
+    
 }
