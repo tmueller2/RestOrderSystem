6137189f57c18eb51903abda510b4af667d25456
Spark <Spark@192.168.1.107>
2012-09-18 15:46:51 -0500

    Updated RestOrderController2.java Still having null pointer error -

--- src/java/edu/wctc/distjava/ch3/controller/RestOrderController2.java
+++ src/java/edu/wctc/distjava/ch3/controller/RestOrderController2.java
@@ -4,6 +4,7 @@ package edu.wctc.distjava.ch3.controller;
 import edu.wctc.distjava.ch3.model.MenuItem;
 import edu.wctc.distjava.ch3.model.OrderService;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -37,7 +38,6 @@ public class RestOrderController2 extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException{
-        response.setContentType("text/html");
         boolean responseCommitted = false;
         //Database connection
         List<MenuItem> menuList = orderService.getMenuList();
@@ -55,7 +55,14 @@ public class RestOrderController2 extends HttpServlet {
                 } 
             }
         }
-        //responseCommitted = outputConfirmation(request, response, menuList, orderList, responseCommitted);
+        if(orderList.size() > 0){
+            orderService.placeOrder();
+        }
+ 
+        // We need to get the value of responseCommitted back from
+        // the method because the original is passed by value (a copy)                    
+        responseCommitted = 
+                outputConfirmation(request, response, menuList, orderList, responseCommitted);
         //if(!responseCommitted) {
         request.setAttribute("menuList", menuList);
         request.setAttribute("orderList", orderList);
@@ -65,6 +72,42 @@ public class RestOrderController2 extends HttpServlet {
                 getServletContext().getRequestDispatcher("/orderResult.jsp");
         dispatcher.forward(request, response);
         //}
+    }
+
+    private boolean outputConfirmation(HttpServletRequest request, HttpServletResponse response, List<MenuItem> menuList, List<MenuItem> orderList, boolean responseCommitted) throws IOException {
+        
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">");
+        out.println("<HTML>");
+        out.println("  <HEAD><TITLE>Order Receipt</TITLE></HEAD>");
+        out.println("  <BODY>");
+        out.print("    <h1>Todd's Sweets & Creamery Order</h2> ");
+        out.println("<p>You ordered the following:</p>");
+        out.println("<ul>");
+        for(MenuItem item : orderList) {
+                out.println("<li>" + item.getMenuDescription() + "</li>");
+        }
+        out.println("</ul>");
+        out.println("<a href='index.jsp'>Place a new order</a>");
+        out.println("  </BODY>");
+        out.println("</HTML>");
+        out.flush();
+        out.close();
+        menuList.addAll(orderList);
+        orderList.removeAll(orderList);
+        responseCommitted = true;
+        
+        return responseCommitted;
+        
+    }
+    /**
+     * Initialize the servlet
+     * @throws ServletException 
+     */
+    @Override
+    public void init()throws ServletException{
+        orderService = new OrderService();
     }
     
 }
